eefddf747fc28c75b5a58bf9592eb8d9795a5128
Stephen Pierzchala <pierzcha@usc.edu>
2020-11-22 13:35:53 -0600

    added basic directory structure for diff routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Checkout from "./Components/Checkout/Checkout";
+import ViewCart from "./Components/ViewCart/ViewCart";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload. Hello
-					World!
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Test
-				</a>
-			</header>
-		</div>
+		<main>
+			<Switch>
+				<Route path="/" component={Home} exact />
+				<Route path="/Login" component={Login} />
+				<Route path="/Checkout" component={Checkout} />
+				<Route path="/ViewCart" component={ViewCart} />
+			</Switch>
+		</main>
 	);
 }
 
