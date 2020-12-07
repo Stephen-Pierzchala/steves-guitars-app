7a689b5b3b2dfc5440af8a9acc6448120e5e6614
Stephen Pierzchala <pierzcha@usc.edu>
2020-12-07 00:04:20 -0600

    remove from cart function complete

--- src/Controllers/Cart.js
+++ src/Controllers/Cart.js
@@ -16,8 +16,8 @@ const addToCart = (id) => {
 		let cart = JSON.parse(oldCart);
 
 		//see if the added item is already in the cart
-		let item = cart.items.find((item) => {
-			return item.id === id;
+		let item = cart.items.find((cartItem) => {
+			return cartItem.id === id;
 		});
 		if (item) item.quantity++;
 		else {
@@ -29,7 +29,28 @@ const addToCart = (id) => {
 	}
 };
 
-const removeFromCart = (id) => {};
+const removeFromCart = (id) => {
+	let cart = localStorage.getItem("cart");
+	if (!cart) return;
+	cart = JSON.parse(cart);
+
+	//search for the item in the cart
+	let item = cart.items.find((cartItem) => {
+		return cartItem.id === id;
+	});
+
+	if (!item) return;
+
+	if (item.quantity === 1) {
+		cart.items = cart.items.filter((cartItem) => {
+			return cartItem.id !== id;
+		});
+	} else {
+		item.quantity--;
+	}
+	console.log(cart);
+	setCart(cart);
+};
 
 const setCart = (cart) => {
 	localStorage.setItem("cart", JSON.stringify(cart));
