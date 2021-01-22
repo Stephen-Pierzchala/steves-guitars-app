c3d211919ef8ac2647824b1413ed33d1973cd5fb
Stephen Pierzchala <pierzcha@usc.edu>
2021-01-21 21:49:41 -0600

    removed extraneous import

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import routes from "../../Routes/Routes";
